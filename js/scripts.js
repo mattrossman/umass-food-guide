068ab4eab75f3fd452f48e42142325aa376ada70
mattrossman <mrossman@umass.edu>
2017-09-09 22:49:13 -0400

    Switched to array storage for properties, streamlined quantitative props, changed name to UMenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
-var qual_props = {
-	'Dish':'data-dish-name',
-	'Serving size':'data-serving-size'
-}
+var qual_props = [
+	'data-dish-name',
+	'data-serving-size'
+]
 
-var int_props = {
-	'Calories':'data-calories',
-}
-
-var unit_props = {
-	'Cholesterol':'data-cholesterol',
-	'Dietary fiber':'data-dietary-fiber',
-	'Protein':'data-protein',
-	'Saturated fat':'data-sat-fat',
-	'Sodium':'data-sodium',
-	'Sugar':'data-sugars',
-	'Carbs':'data-total-carb',
-	'Total fat':'data-total-fat',
-	'Trans fat':'data-trans-fat'
-}
+var quant_props = [
+	'data-calories',
+	'data-cholesterol',
+	'data-dietary-fiber',
+	'data-protein',
+	'data-sat-fat',
+	'data-sodium',
+	'data-sugars',
+	'data-total-carb',
+	'data-total-fat',
+	'data-trans-fat'
+]
 
 function getMeal(element){
 	return $(element).parent().parent().prevAll('h2').first().text()
@@ -30,34 +27,23 @@ function getSection(element){
 // Get the nutritional data from a given <a> element
 function getElementData(element){
 	var payload = {}
-	// Just return the qualitative properties as strings
-	$.each(qual_props, function(lbl, attr){
-		payload[lbl] = $(element).attr(attr);
+	// Save the qualitative properties as regular strings
+	$.each(qual_props, function(i,attr){
+		payload[attr] = $(element).attr(attr);
 	});
-	// Parse the integer based properties as ints
-	$.each(int_props, function(lbl, attr){
-		payload[lbl] = parseInt($(element).attr(attr));
+	// Parse the quantitative properties as quantities.js objects
+	$.each(quant_props, function(i,attr){
+		payload[attr] = Qty($(element).attr(attr));
 	});
-	// Parse the unit based properties as math.unit objects
-	$.each(unit_props, function(lbl, attr){
-		try {
-			payload[lbl] = Qty($(element).attr(attr));
-		}
-		catch(err){
-			payload[lbl] = Qty(0)
-		}
-	});
-	// These last two properties are not implicit to the element
-	payload['Meal'] = getMeal(element);
-	payload['Section'] = getSection(element);
+	// The meal and section properties are not implicit to the element
+	payload['data-meal'] = getMeal(element);
+	payload['data-section'] = getSection(element);
 	return payload;
 }
 
 function getURL(loc){
-	return "https://umassdining.com/locations-menus/"+loc+"/menu";
+	return "http://umassdining.com/locations-menus/"+loc+"/menu";
 }
-
-var exJSON;
 
 function getLocData(loc){
 	$.get(getURL(loc), function(data){
@@ -66,7 +52,7 @@ function getLocData(loc){
 		var dishObj;
 		var locObj = $('.lightbox-nutrition :first-child',data).map(function(){
 			dishObj =  getElementData(this);
-			dishObj['Location'] = loc;
+			dishObj['data-location'] = loc;
 			return dishObj;
 		}).get();
 		appendData(locObj);
@@ -74,13 +60,13 @@ function getLocData(loc){
 }
 
 function appendData(data){
-	foodObj = foodObj.concat(data)
-    $("#food-table").tabulator("setData", foodObj);
+	tableData = tableData.concat(data)
+    $("#food-table").tabulator("setData", tableData);
 }
 
 
 $(document).ready(function(){
-	foodObj = [];
+	tableData = [];
 	getLocData('Hampshire');
 	getLocData('Berkshire');
 	getLocData('Worcester');
@@ -92,14 +78,14 @@ $(document).ready(function(){
 	    //height:205, // set height of table
 	    fitColumns:true, //fit columns to width of table (optional)
 	    columns:[ //Define Table Columns
-	        {title:"Dish", field:"Dish"},
-	        {title:"Serving size", field:"Serving size"},
-	        {title:"Location", field:"Location"},
-	        {title:"Meal", field:"Meal"},
-	        {title:"Section", field:"Section"},
-	        {title:"Calories", field:"Calories", align:"left"},
-	        {title:"Protein", field:"Protein", align:"left", sorter:unitSorter},
-	        {title:"Sodium", field:"Sodium", align:"left", sorter:unitSorter}
+	        {title:"Dish", field:"data-dish-name"},
+	        {title:"Serving size", field:"data-serving-size"},
+	        {title:"Location", field:"data-location"},
+	        {title:"Meal", field:"data-meal"},
+	        {title:"Section", field:"data-section"},
+	        {title:"Calories", field:"data-calories", align:"left"},
+	        {title:"Protein", field:"data-protein", align:"left", sorter:unitSorter},
+	        {title:"Sodium", field:"data-sodium", align:"left", sorter:unitSorter}
     	],
 	});
 });
