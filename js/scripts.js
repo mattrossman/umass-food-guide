ec2792e3958f1c4af90faa61d33557e27e7b399f
mattrossman <mrossman@umass.edu>
2017-09-09 23:38:55 -0400

    Generalized tabulator column configurations to allow easy selection of certain properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-var qual_props = [
-	'data-dish-name',
-	'data-serving-size'
+var qual_attrs = [
+'data-dish-name',
+'data-serving-size'
 ]
 
-var quant_props = [
-	'data-calories',
-	'data-cholesterol',
-	'data-dietary-fiber',
-	'data-protein',
-	'data-sat-fat',
-	'data-sodium',
-	'data-sugars',
-	'data-total-carb',
-	'data-total-fat',
-	'data-trans-fat'
+var quant_attrs = [
+'data-calories',
+'data-cholesterol',
+'data-dietary-fiber',
+'data-protein',
+'data-sat-fat',
+'data-sodium',
+'data-sugars',
+'data-total-carb',
+'data-total-fat',
+'data-trans-fat'
 ]
 
 function getMeal(element){
@@ -28,11 +28,11 @@ function getSection(element){
 function getElementData(element){
 	var payload = {}
 	// Save the qualitative properties as regular strings
-	$.each(qual_props, function(i,attr){
+	$.each(qual_attrs, function(i,attr){
 		payload[attr] = $(element).attr(attr);
 	});
 	// Parse the quantitative properties as quantities.js objects
-	$.each(quant_props, function(i,attr){
+	$.each(quant_attrs, function(i,attr){
 		payload[attr] = Qty($(element).attr(attr));
 	});
 	// The meal and section properties are not implicit to the element
@@ -64,28 +64,44 @@ function appendData(data){
     $("#food-table").tabulator("setData", tableData);
 }
 
-
 $(document).ready(function(){
+	$( "#accordion" ).accordion({
+		animate:200,
+		collapsible:true,
+		active:false
+	});
 	tableData = [];
 	getLocData('Hampshire');
-	getLocData('Berkshire');
-	getLocData('Worcester');
-	getLocData('Franklin');
+	//getLocData('Berkshire');
+	//getLocData('Worcester');
+	//getLocData('Franklin');
 	var unitSorter = function(a,b){
 		return a.baseScalar-b.baseScalar;
 	}
+	allColumns = [
+		{title:"Dish", field:"data-dish-name"},
+		{title:"Serving size", field:"data-serving-size"},
+		{title:"Location", field:"data-location"},
+		{title:"Meal", field:"data-meal"},
+		{title:"Section", field:"data-section"},
+		{title:"Calories", field:"data-calories", align:"left"},
+		{title:"Cholesterol", field:"data-cholesterol", align:"left", sorter:unitSorter},
+		{title:"Dietary fiber", field:"data-dietary-fiber", align:"left", sorter:unitSorter},
+		{title:"Protein", field:"data-protein", align:"left", sorter:unitSorter},
+		{title:"Saturated fat", field:"data-sat-fat", align:"left", sorter:unitSorter},
+		{title:"Sodium", field:"data-sodium", align:"left", sorter:unitSorter},
+		{title:"Sugar", field:"data-sugars", align:"left", sorter:unitSorter},
+		{title:"Carbohydrates", field:"data-total-carb", align:"left", sorter:unitSorter},
+		{title:"Total fat", field:"data-total-fat", align:"left", sorter:unitSorter},
+		{title:"Trans fat", field:"data-trans-fat", align:"left", sorter:unitSorter}
+	]
+	var activeColumns = ["Dish", "Serving size", "Meal", "Section", "Calories"]
 	$("#food-table").tabulator({
 	    //height:205, // set height of table
 	    fitColumns:true, //fit columns to width of table (optional)
-	    columns:[ //Define Table Columns
-	        {title:"Dish", field:"data-dish-name"},
-	        {title:"Serving size", field:"data-serving-size"},
-	        {title:"Location", field:"data-location"},
-	        {title:"Meal", field:"data-meal"},
-	        {title:"Section", field:"data-section"},
-	        {title:"Calories", field:"data-calories", align:"left"},
-	        {title:"Protein", field:"data-protein", align:"left", sorter:unitSorter},
-	        {title:"Sodium", field:"data-sodium", align:"left", sorter:unitSorter}
-    	],
-	});
+	    columns:$.grep(allColumns,function(obj,i){
+	    	// Filter only the column object configuations with titles in the active list
+	    	return $.inArray(obj.title,activeColumns)!=-1
+	    })
+    });
 });
