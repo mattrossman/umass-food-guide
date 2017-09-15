6eccc5d28a76d0d9f0a0c6cf5b235d5e4e5c0d6c
mattrossman <mrossman@umass.edu>
2017-09-15 14:34:06 -0400

    Lay framework for column adder
    Planning on implementing Bootstrap before finishing the column adder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var tableColumns;
+
 var qual_attrs = [
 "data-dish-name",
 "data-serving-size"
@@ -16,6 +18,28 @@ var quant_attrs = [
 "data-trans-fat"
 ]
 
+var unitSorter = function(a,b){
+	return a.baseScalar-b.baseScalar;
+};
+
+var allColumns = [
+{title:"Dish", 			field:"data-dish-name"},
+{title:"Serving size", 	field:"data-serving-size",	headerSort:false},
+{title:"Location", 		field:"data-location"},
+{title:"Meal", 			field:"data-meal"},
+{title:"Section",		field:"data-section"},
+{title:"Calories", 		field:"data-calories", 		align:"left"},
+{title:"Cholesterol", 	field:"data-cholesterol",	align:"left", sorter:unitSorter},
+{title:"Dietary fiber", field:"data-dietary-fiber", align:"left", sorter:unitSorter},
+{title:"Protein", 		field:"data-protein", 		align:"left", sorter:unitSorter},
+{title:"Saturated fat", field:"data-sat-fat", 		align:"left", sorter:unitSorter},
+{title:"Sodium", 		field:"data-sodium", 		align:"left", sorter:unitSorter},
+{title:"Sugar", 		field:"data-sugars", 		align:"left", sorter:unitSorter},
+{title:"Carbohydrates", field:"data-total-carb", 	align:"left", sorter:unitSorter},
+{title:"Total fat", 	field:"data-total-fat", 	align:"left", sorter:unitSorter},
+{title:"Trans fat", 	field:"data-trans-fat", 	align:"left", sorter:unitSorter}
+];
+
 // I don't know what this stands for, but it's used in the online menu
 // as an ID for each dining common
 var tids = {
@@ -25,34 +49,47 @@ var tids = {
 	Berkshire:4
 }
 
+
+// Gets a parameter from the URL query
+// ex: www.mysite.com/?param1=val1&param2=val2
+// getParameterByName("param1") ==> "val1"
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// Find the first key for a certain value in an object
 function getKey(obj,value) {
-  return Object.keys(obj).find(key => obj[key] === value);
+	return Object.keys(obj).find(key => obj[key] === value);
 };
 
-// Get the implicit nutritional data from a given <a> element
+// Get the implicit nutritional data from a given <a> element.
+// These data values are initially stored as element properties.
 function getImplicitData(element){
 	var payload = {}
 	// Save the qualitative properties as regular strings
-	$.each(qual_attrs, function(i,attr){
+	$.map(qual_attrs, function(attr){
 		payload[attr] = $(element).attr(attr);
 	});
 	// Parse the quantitative properties as quantities.js objects
-	$.each(quant_attrs, function(i,attr){
+	$.map(quant_attrs, function(attr){
 		payload[attr] = Qty($(element).attr(attr));
 	});
 	return payload;
 }
 
-function getURL(loc){
-	return "http://umassdining.com/locations-menus/"+loc+"/menu";
-}
-
-function getNewURL(tid, date){
+// Builds the AJAX request URL for the UMass menu server
+function getURL(tid, date){
 	return "http://umassdining.com/foodpro-menu-ajax?tid="+tid+"&date="+date;
 }
 
 function addData(tid,date){
-	$.get(getNewURL(tid,date), function(data){
+	$.get(getURL(tid,date), function(data){
 		// The first element of each 'lightbox-nutrition' <li> is an <a>
 		// with the nutritional data attributes
 		locObj = JSON.parse(data);
@@ -97,45 +134,52 @@ function settingsHandler(){
 	getLocData(tid,date);
 }
 
+function removeColumnOption(col){
+	$("#col-adder").find(":contains("+col+")").remove()
+}
+
+function addColumn(col){
+	tableColumns = tableColumns.concat(col);
+	removeColumnOption(col);
+	updateColumns();
+}
+
+function initColumnAdder(){
+	var colAdder = $("#col-adder")
+	$.map(allColumns,function(col){
+		colAdder.append("<option>"+col.title+"</option>");
+	})
+}
+
+// Pulls the appropriate column objects from the master list
+// according to the titles currently specified in `tableColumns`
+// and sends them to the tabulator element.
+function updateColumns(){
+	$("#food-table").tabulator("setColumns",
+		$.map(tableColumns,function(prop){
+			return allColumns.find(col => col.title === prop)
+		}));
+}
+
 $(document).ready(function(){
 	$( "#accordion" ).accordion({
 		animate:200,
-		collapsible:true,
-		active:false
+		collapsible:true
 	});
-	$("#datepicker").datepicker({
-		onSelect:settingsHandler
-	});
+	$("#datepicker").datepicker();
+
+	// Sets the default date to today
 	$("#datepicker").datepicker("setDate", new Date());
-	$("#dc-selector").change(settingsHandler);
-	//getLocData('Hampshire');
-	var unitSorter = function(a,b){
-		return a.baseScalar-b.baseScalar;
-	};
-	allColumns = [
-		{title:"Dish", 			field:"data-dish-name"},
-		{title:"Serving size", 	field:"data-serving-size", headerSort:false},
-		{title:"Location", 		field:"data-location"},
-		{title:"Meal", 			field:"data-meal"},
-		{title:"Section",		field:"data-section"},
-		{title:"Calories", 		field:"data-calories", align:"left"},
-		{title:"Cholesterol", 	field:"data-cholesterol", align:"left", sorter:unitSorter},
-		{title:"Dietary fiber", field:"data-dietary-fiber", align:"left", sorter:unitSorter},
-		{title:"Protein", 		field:"data-protein", align:"left", sorter:unitSorter},
-		{title:"Saturated fat", field:"data-sat-fat", align:"left", sorter:unitSorter},
-		{title:"Sodium", 		field:"data-sodium", align:"left", sorter:unitSorter},
-		{title:"Sugar", 		field:"data-sugars", align:"left", sorter:unitSorter},
-		{title:"Carbohydrates", field:"data-total-carb", align:"left", sorter:unitSorter},
-		{title:"Total fat", 	field:"data-total-fat", align:"left", sorter:unitSorter},
-		{title:"Trans fat", 	field:"data-trans-fat", align:"left", sorter:unitSorter}
-	];
-	var activeColumns = ["Dish", "Calories", "Serving size", "Meal", "Section"]
+
+	tableColumns = [];
+	initColumnAdder();
+
 	$("#food-table").tabulator({
-	    //height:205, // set height of table
-	    fitColumns:false, //fit columns to width of table (optional)
-    	// Find the column config objects with the desired titles
-	    columns:$.map(activeColumns,function(prop){
-	    	return allColumns.find(col => col.title === prop)
-	    })
+		responsive: true,
+		movableColumns: true,
     });
+
+    // Add a few starter columns to the table
+	var defaultColumns = ["Dish", "Calories", "Serving size", "Meal", "Section"]
+	$.map(defaultColumns,addColumn);
 });
